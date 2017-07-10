5ab0bab7ba707e01396c5a87c0c58e0058ac0c91
May Cui <mayc@fb.com>
2017-07-10 12:25:48 -0700

    added tab layout and adapter to NewsFeedActivity, imported design library

--- app/src/main/java/com/example/mayc/openmind/NewsfeedActivity.java
+++ app/src/main/java/com/example/mayc/openmind/NewsfeedActivity.java
@@ -1,14 +1,31 @@
 package com.example.mayc.openmind;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.TabLayout;
+
+
+import com.example.mayc.openmind.fragments.TimelineAdapter;
 
 public class NewsfeedActivity extends AppCompatActivity {
+
+    TimelineAdapter adapterView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newsfeed);
+
+        //setting the adapter for the page
+        ViewPager vp = (ViewPager) findViewById(R.id.viewpager);
+        adapterView = new TimelineAdapter(getSupportFragmentManager(), this);
+        vp.setAdapter(adapterView);
+
+        //setting the tab layout
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(vp);
 
     }
 }
