c724f80998acdd9f04f724ccde15c2dcf51ba6b4
ellcor <ecorbus@college.harvard.edu>
2017-07-11 12:25:24 -0700

    In the middle of working on Article.java

--- app/src/main/java/com/example/mayc/openmind/models/Article.java
+++ app/src/main/java/com/example/mayc/openmind/models/Article.java
@@ -1,8 +1,53 @@
 package com.example.mayc.openmind.models;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by mayc on 7/10/17.
  */
 
 public class Article {
+
+    // article attributes
+    public String title;
+    public String body;
+    public String source;
+    public String author;
+    public String topic;
+    public String datePublished;
+    public String sentiment;
+
+    public String imageUrl;
+
+    // deserialize the JSON
+    // TODO
+    public static Article fromJSON(JSONObject jsonObject) throws JSONException {
+
+        Article articleItem = new Article();
+
+        // extract the values from JSON
+        articleItem.body = jsonObject.getString("text");
+        articleItem.
+        articleItem.
+        articleItem.
+
+        return articleItem;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public long getUid() {
+        return uid;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
 }
