71570b39327c4f845160965e81f66441277bd67a
decam9 <decam9@fb.com>
2017-07-13 14:28:36 -0700

    Added article constructor. Still missing some information

--- app/src/main/java/com/example/mayc/openmind/models/Article.java
+++ app/src/main/java/com/example/mayc/openmind/models/Article.java
@@ -1,5 +1,7 @@
 package com.example.mayc.openmind.models;
 
+import com.example.mayc.openmind.DocumentPayload;
+
 /**
  * Created by mayc on 7/10/17.
  */
@@ -15,11 +17,24 @@ public class Article {
     public String topic;
     public String datePublished;
     public String sentiment;
+    public String bodySnippet;
 
 
-    Article() {
+
+    //TODO: Devon Create a way to get the commented info from Document payload
+    public Article(DocumentPayload d) {
         //will take information from document payload and extract values to set as attributes
+        title = d.getTitle();
+        body = d.getBody();
+        bodySnippet = d.getBodySnippet();
+        source = d.getSourceUrl();
+//        author = d.Author;
+//        topic = pTopic;
+//        datePublished = pDatePublished;
+//        sentiment = pSentiment;
     }
+
+
 
     public String getTitle() {
         return title;
@@ -38,14 +53,17 @@ public class Article {
     }
 
     public String getTopic() {
+
         return topic;
     }
 
     public String getDatePublished() {
+
         return datePublished;
     }
 
     public String getSentiment() {
+
         return sentiment;
     }
 }
