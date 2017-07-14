91b310dfae9ada2d2dedccab1e0ecd888aa92991
May Cui <mayc@berkeley.edu>
2017-07-14 11:18:16 -0700

    set up onclick listener, not tested yet

--- app/src/main/java/com/example/mayc/openmind/StartActivity.java
+++ app/src/main/java/com/example/mayc/openmind/StartActivity.java
@@ -1,17 +1,31 @@
 package com.example.mayc.openmind;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /* this screen will launch for users who have no information, will include start button that leads to SurveyActivity*/
 
 public class StartActivity extends AppCompatActivity {
 
+    Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        context = this;
 
-        //TODO: set onClick listener for startButton that will start SurveyActivity
+        Button start = (Button)findViewById(R.id.startButton);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(context, SurveyActivity.class);
+                context.startActivity(i);
+            }
+        });
     }
 }
