215e710782f807f3521ff9fc7f9145d0d7f5bdd9
decam9 <decam9@fb.com>
2017-07-14 22:15:17 -0700

    Created and casted views in the viewholder. Next, i will bind the views.

--- app/src/main/java/com/example/mayc/openmind/ArticleAdapter.java
+++ app/src/main/java/com/example/mayc/openmind/ArticleAdapter.java
@@ -5,6 +5,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.mayc.openmind.models.Article;
 
@@ -37,11 +39,13 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHold
         View articleView = inflater.inflate(R.layout.item_article, parent, false);
         ViewHolder viewHolder = new ViewHolder(articleView);
         return viewHolder;
+
     }
 
+    //Binds the retrieved info to the view based on position
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-
+        //TODO: Bind retrieved information with the appropriate data from client
     }
 
     @Override
@@ -49,19 +53,43 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHold
         return mArticles.size();
     }
 
-    //TODO: bind and set views from item_article.xml
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public class ViewHolder extends RecyclerView.ViewHolder  {
+        public ImageView ivPublisherImage;
+        public TextView tvPublisher;
+        public TextView tvDateCreated;
+        public ImageView ivCategoryIcon;
+        public ImageView ivBookmarkIcon;
+        public ImageView ivArticleImage;
+        public TextView tvTitle;
+        public TextView tvDescription;
+        public TextView tvSource;
+        public TextView tvAuthor;
+
 
         public ViewHolder(View itemView) {
             super(itemView);
 
+            //this performs the lookups
+
+            ivPublisherImage = (ImageView) itemView.findViewById(R.id.ivPublisherImage);
+            tvPublisher = (TextView) itemView.findViewById(R.id.tvPublisher);
+            tvDateCreated = (TextView) itemView.findViewById(R.id.tvDateCreated);
+            ivCategoryIcon = (ImageView) itemView.findViewById(R.id.ivCategoryIcon);
+            ivBookmarkIcon = (ImageView) itemView.findViewById(R.id.ivBookmarkIcon);
+            ivArticleImage = (ImageView) itemView.findViewById(R.id.ivArticleImage);
+            tvTitle = (TextView) itemView.findViewById(R.id.tvTitle);
+            tvDescription = (TextView) itemView.findViewById(R.id.tvDescription);
+            tvSource = (TextView) itemView.findViewById(R.id.tvSource);
+            tvAuthor = (TextView) itemView.findViewById(R.id.tvAuthor);
+
+            //Handles the clicks for opening articles
+            itemView.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View view) {
+                    //TODO Devon Implement on click
+                }
+            });
         }
-
-        @Override
-        public void onClick(View v) {
-
-        }
-
-
     }
 }
