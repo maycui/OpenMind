ecf39b8df901c872247dab6020ed77dbfd3772e5
ellcor <ecorbus@college.harvard.edu>
2017-07-20 15:58:39 -0700

    Made minor changes to user info json, continued working on ArticlesProvider

--- app/src/main/java/com/example/mayc/openmind/ArticlesProvider.java
+++ app/src/main/java/com/example/mayc/openmind/ArticlesProvider.java
@@ -1,26 +1,15 @@
 package com.example.mayc.openmind;
 
-import java.util.Arrays;
-import java.util.HashSet;
-
 import android.content.ContentProvider;
-import android.content.ContentUris;
 import android.content.ContentValues;
-import android.content.Context;
 import android.content.UriMatcher;
 import android.database.Cursor;
-import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
-import android.text.TextUtils;
-import com.example.mayc.openmind.DatabaseHandler;
-import com.example.mayc.openmind.ArticlesTable;
-
-import java.util.HashMap;
 
 import static android.provider.MediaStore.Audio.Playlists.Members._ID;
+import static com.example.mayc.openmind.ArticlesTable.TABLE_NAME;
 import static com.example.mayc.openmind.DatabaseHandler.DATABASE_NAME;
 
 /**
@@ -33,6 +22,7 @@ public class ArticlesProvider extends ContentProvider {
     private DatabaseHandler database;
 
     // used for the UriMatcher
+    // TODO: figure out what these are
     static final int ARTICLES = 1;
     static final int ARTICLE_ID = 2;
 
@@ -44,42 +34,14 @@ public class ArticlesProvider extends ContentProvider {
             + "/" + BASE_PATH);
 
 
-//    TODO: figure out what STUDENTS and STUDENT_ID are
-//    static final int STUDENTS = 1;
-//    static final int STUDENT_ID = 2;
-
-
-
-    // TODO: Figure out how to handle URI IDs (what should it be?)
-    // What exactly is this section doing?
-
+    // TODO: Figure out what exactly this section is doing
     private static final UriMatcher uriMatcher = new UriMatcher(
             UriMatcher.NO_MATCH
     );
     static{
-        uriMatcher.addURI(AUTHORITY, BASE_PATH, ARTICLES); // Isn't the int key optional? How to fix this?
+        uriMatcher.addURI(AUTHORITY, BASE_PATH, ARTICLES);
         uriMatcher.addURI(AUTHORITY, BASE_PATH + "/#", ARTICLE_ID);
     }
-
-    /**
-     * Database specific constant declarations
-     */
-
-//    private SQLiteDatabase db;
-//    static final String DATABASE_NAME = "ArticleDatabase";
-//    static final String ARTICLES_TABLE_NAME = "articles";
-//    static final int DATABASE_VERSION = 1;
-//    // Creating the DB table is already done in the DB Handler...how to deal with this?
-//    static final String CREATE_DB_TABLE =
-//            " CREATE TABLE " + ARTICLES_TABLE_NAME +
-//                    " (_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-//                    " name TEXT NOT NULL, " +
-//                    " grade TEXT NOT NULL);";
-
-    /**
-     * Helper class that actually creates and manages
-     * the provider's underlying data repository.
-     */
 
 
     @Override
@@ -88,7 +50,7 @@ public class ArticlesProvider extends ContentProvider {
         database = new DatabaseHandler(getContext(), DATABASE_NAME);
 
         // return false if the database is null
-        return (database == null)? false:true;
+        return database != null;
     }
 
 
@@ -101,30 +63,16 @@ public class ArticlesProvider extends ContentProvider {
         long id = 0;
         switch (uriType) {
             case ARTICLES:
-                id = sqlDB.insert(Ar)
+                id = sqlDB.insert(TABLE_NAME, null, values);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI: " + uri);
+        }
 
-
-
-
-
-//
-//
-//        /**
-//         * Add a new student record
-//         */
-//        long rowID = db.insert(ARTICLES_TABLE_NAME, "", values);
-//
-//        /**
-//         * If record is added successfully
-//         */
-//        if (rowID > 0) {
-//            Uri _uri = ContentUris.withAppendedId(CONTENT_URI, rowID);
-//            getContext().getContentResolver().notifyChange(_uri, null);
-//            return _uri;
-//        }
-//
-//        throw new SQLException("Failed to add a record into " + uri);
+        getContext().getContentResolver().notifyChange(uri, null);
+        return Uri.parse(BASE_PATH + "/" + id);
     }
+
 
     @Override
     public Cursor query(Uri uri, String[] projection,
@@ -133,7 +81,7 @@ public class ArticlesProvider extends ContentProvider {
         SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
 
         // set the table (setTables sets the list of tables to query)
-        qb.setTables(ARTICLES_TABLE_NAME);
+        qb.setTables(TABLE_NAME);
 
         // TODO: figure out this section
         int uriType = uriMatcher.match(uri);
@@ -144,7 +92,7 @@ public class ArticlesProvider extends ContentProvider {
             case ARTICLE_ID:
                 // adding the ID to the original query
                 // TODO: figure out this line
-                qb.appendWhere( _ID + "=" + uri.getLastPathSegment().get(1));
+                qb.appendWhere(_ID + "=" + uri.getLastPathSegment());
                 break;
 
             default:
@@ -176,19 +124,4 @@ public class ArticlesProvider extends ContentProvider {
     public String getType(Uri uri) {
         return null;
     }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
