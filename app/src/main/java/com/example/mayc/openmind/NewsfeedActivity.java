ce9c581582b3aab7ce66b2b081ad6aee1cdf1647
May Cui <mayc@berkeley.edu>
2017-07-20 10:01:31 -0700

    newsfeed activity now has working tabs and hamburger menu

--- app/src/main/java/com/example/mayc/openmind/NewsfeedActivity.java
+++ app/src/main/java/com/example/mayc/openmind/NewsfeedActivity.java
@@ -2,9 +2,14 @@ package com.example.mayc.openmind;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,7 +17,7 @@ import com.example.mayc.openmind.fragments.TimelineAdapter;
 
 //TODO: get action bar
 
-public class NewsfeedActivity extends AppCompatActivity {
+public class NewsfeedActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener  {
 
     TimelineAdapter adapterView;
 
@@ -22,20 +27,34 @@ public class NewsfeedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newsfeed);
-
         //setting the adapter for the page
         ViewPager vp = (ViewPager) findViewById(R.id.viewpager);
         adapterView = new TimelineAdapter(getSupportFragmentManager(), this);
         vp.setAdapter(adapterView);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
         //setting the tab layout
         TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
         tabLayout.setupWithViewPager(vp);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
+
         return true;
     }
 
@@ -49,6 +68,16 @@ public class NewsfeedActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
 
     }
 }
