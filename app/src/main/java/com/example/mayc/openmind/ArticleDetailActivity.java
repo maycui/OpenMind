e46bd1a59be2599dc69b3690cacbab1e9fd3fb5e
decam9 <decam9@fb.com>
2017-07-20 16:06:26 -0700

    Added butterknife to start setting and binding

--- app/src/main/java/com/example/mayc/openmind/ArticleDetailActivity.java
+++ app/src/main/java/com/example/mayc/openmind/ArticleDetailActivity.java
@@ -2,21 +2,45 @@ package com.example.mayc.openmind;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.mayc.openmind.models.Article;
 
 import org.parceler.Parcels;
 
+import butterknife.BindView;
+
 public class ArticleDetailActivity extends AppCompatActivity {
 
     Article article;
+
+    @BindView (R.id.tvPublisher) TextView tvPublisher;
+    @BindView(R.id.tvArticleTitle) TextView tvArticleTitle;
+    @BindView(R.id.tvAuthor) TextView tvAuthor;
+    @BindView(R.id.tvImageCaption) TextView tvImageCaption;
+    @BindView(R.id.tvArticleUrl) TextView TvArticleUrl;
+    @BindView(R.id.ivArticleImage) ImageView ivArticleImage;
+    @BindView(R.id.ivOpenIcon) ImageView ivOpenIcon;
+    @BindView(R.id.tvArticleDescription) TextView tvArticleDescription;
+    @BindView(R.id.tvDatePublished) TextView tvDatePublished;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detail_article);
-
         article =  Parcels.unwrap(getIntent().getParcelableExtra(Article.class.getSimpleName()));
+        Log.d("ArticleDetailActivity", String.format("Showing details for '%s'", article.getID()));
+
+
+        tvArticleTitle.setText(article.title);
+        tvAuthor.setText(article.author);
+        tvArticleDescription.setText(article.bodySnippet);
+
+
+
+
 
     }
 }
