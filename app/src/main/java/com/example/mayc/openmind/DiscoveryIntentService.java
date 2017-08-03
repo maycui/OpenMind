70be8298edd5a2c9100597bc8368891ec54d9aec
May Cui <mayc@berkeley.edu>
2017-08-03 15:08:46 -0700

    added income to prefs hashmap and started working on info activity

--- app/src/main/java/com/example/mayc/openmind/DiscoveryIntentService.java
+++ app/src/main/java/com/example/mayc/openmind/DiscoveryIntentService.java
@@ -46,14 +46,6 @@ public class DiscoveryIntentService extends IntentService {
 
         List<Article> result = null;
 
-        //LOAD PREFERNECES
-
-//        public static final String GENDER = "Gender";
-//        public static final String INCOME = "Income";
-//        public static final String RACE = "Race/Ethnicity";
-//        public static final String SEX = "Sexual Orientation";
-//        public static final String DISABILITY = "Are you legally disabled?";
-
         pref = new HashMap<>();
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
         pref.put(Constants.NAME, sharedPref.getString(Constants.NAME, ""));
@@ -62,13 +54,14 @@ public class DiscoveryIntentService extends IntentService {
         pref.put(Constants.RACE, sharedPref.getString(Constants.RACE, ""));
         pref.put(Constants.SEX, sharedPref.getString(Constants.SEX, ""));
         pref.put(Constants.DISABILITY, sharedPref.getString(Constants.DISABILITY, ""));
+        pref.put(Constants.INCOME, sharedPref.getString(Constants.INCOME, ""));
+
 
 
         query = QueryBuilder.getKWs4Prefs(pref);
 
-        //calling the api and receiving an array of document payloads as a response
-        //api link should be: https://gateway.watsonplatform.net/discovery/api/v1/environments/d83ace81-1722-4566-bd55-47417607f2b1/collections/789640cf-7e1a-4f21-bc88-4c0e7f4824a0/query?version=2017-06-25
         DiscoveryClient test = new DiscoveryClient();
+
         try {
             result = test.getArticles(query);
         } catch (Exception e) {
