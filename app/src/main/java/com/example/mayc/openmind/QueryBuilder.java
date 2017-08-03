4206242fc180784bdcf0661de102de5e6e0c74ed
ellcor <ecorbus@college.harvard.edu>
2017-08-03 11:34:44 -0700

    Created query builder class

---
+++ app/src/main/java/com/example/mayc/openmind/QueryBuilder.java
@@ -0,0 +1,16 @@
+package com.example.mayc.openmind;
+
+import java.util.Map;
+
+/**
+ * Created by elliecorbus on 8/3/17.
+ */
+
+public class QueryBuilder {
+
+    public String getKWs4Prefs(Map<String, String> prefs) {
+
+        return "Not yet implemented";
+
+    }
+}
