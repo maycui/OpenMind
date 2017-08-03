6e599c2af9603fdb41eb74b56aedd3b940589f5a
ellcor <ecorbus@college.harvard.edu>
2017-08-03 13:58:37 -0700

    Added to Query Builder function and made changes to user info

--- app/src/main/java/com/example/mayc/openmind/QueryBuilder.java
+++ app/src/main/java/com/example/mayc/openmind/QueryBuilder.java
@@ -12,45 +12,100 @@ public class QueryBuilder {
 
         String query = "";
 
-        // gender category
-        if (prefs.get("gender").equals("male")) {
-            query += "female, ";
+        /*
+            GENDER CATEGORY
+         */
+
+        String prefsGender = prefs.get("gender");
+
+        // cis males
+        if (prefsGender.equals("Cis male")) {
+            query += "female, transgender, genderfluid, ";
         }
-        if (prefs.get("gender").equals("female")) {
-            query += "transgender";
+
+        // cis females
+        if (prefsGender.equals("Cis female")) {
+            query += "transgender, genderfluid, ";
         }
 
-        // age category
+        /*
+            AGE CATEGORY
+         */
+
+        int intAge = Integer.parseInt(prefs.get("age"));
+
+        // teenagers:
+        if (intAge < 20) {
+            query += "middle age, middle-aged, senior, seniors, old age, ";
+        }
+
+        // middle-aged:
+        else if (intAge >= 40 && intAge < 65) {
+            query += "teenager, teenagers, senior, seniors, old age, ";
+        }
+
+        // seniors:
+        else if (intAge >= 65) {
+            query += "teenager, teenagers, ";
+        }
+
+        /*
+            SEXUAL ORIENTATION CATEGORY
+         */
+
+        String prefsSO = prefs.get("sexual orientation");
+
+        // heterosexuals:
+        if (prefsSO.equals("heterosexual")) {
+            query += "homosexual, gay, lesbian, bisexual, pansexual, asexual, ";
+        }
+
+        // other:
+        if (prefsSO.equals("homosexual")
+                || prefsSO.equals("bisexual")
+                || prefsSO.equals("pansexual")) {
+            query += "asexual, ";
+        }
+
+        /*
+
+        Income groups:
+
+        "Less than $20k",
+        "$20k to $34k",
+        "$35k to $49k",
+        "$50k to $74k",
+        "$75k to $99k",
+        "Over $100k"
+
+         */
+
+        /*
+            INCOME CATEGORY
+         */
+
+        if (prefs.get("income").equals("Over $100k")) {
+            query += "poverty, ";
+        }
+
+        /*
+            RACE & ETHNICITY CATEGORY
+         */
+
+        if (prefs.get("race/ethnicity").equals("white")) {
+            query += "race, racism, racial bias, ";
+        }
+
+        /*
+            DISABILITY CATEGORY
+         */
+
+        if (prefs.get("disability").equals("no")) {
+            query += "disability, disabilities, ";
+        }
 
 
-        // sexual orientation category
-
-
-        // income category
-
-
-        // race/ethnicity category
-
-
-        // disability category
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        return "Not yet implemented";
+        return query;
 
     }
 }
