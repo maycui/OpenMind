95e635cda801e3f736ba1b13ecbad3297e379e92
ellcor <ecorbus@college.harvard.edu>
2017-08-03 12:09:48 -0700

    Made changed to queryBuilder (including making it static)

--- app/src/main/java/com/example/mayc/openmind/QueryBuilder.java
+++ app/src/main/java/com/example/mayc/openmind/QueryBuilder.java
@@ -6,9 +6,49 @@ import java.util.Map;
  * Created by elliecorbus on 8/3/17.
  */
 
-public class QueryBuilder {
+public static class QueryBuilder {
 
     public String getKWs4Prefs(Map<String, String> prefs) {
+
+        String query = "";
+
+        // gender category
+        if (prefs.get("gender").equals("male")) {
+            query += "female, ";
+        }
+        if (prefs.get("gender").equals("female")) {
+            query += "transgender";
+        }
+
+        // age category
+
+
+        // sexual orientation category
+
+
+        // income category
+
+
+        // race/ethnicity category
+
+
+        // disability category
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         return "Not yet implemented";
 
