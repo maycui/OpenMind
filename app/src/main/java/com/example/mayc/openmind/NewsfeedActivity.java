2474fc88c680894d8aaaac9b9425bd214dc19130
ellcor <ecorbus@college.harvard.edu>
2017-08-08 11:22:14 -0700

    Added name to nav bar header

--- app/src/main/java/com/example/mayc/openmind/NewsfeedActivity.java
+++ app/src/main/java/com/example/mayc/openmind/NewsfeedActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.example.mayc.openmind.fragments.TimelineAdapter;
 
@@ -109,6 +110,15 @@ public class NewsfeedActivity extends AppCompatActivity implements NavigationVie
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
+
+        // retrieve user's name
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        String name = sharedPref.getString(Constants.NAME, "");
+
+        // place user's name in nav bar header
+        TextView tvName = (TextView)findViewById(R.id.tvName);
+        tvName.setText(name);
+
         return true;
     }
 
